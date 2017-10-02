813d449116674193364b54303ae08c343d70662f
mrgerwin <jgerwin@otsegoknights.org>
2017-10-02 10:05:14 -0400

    Add Files from Connor's Computer

---
+++ DatetimeProgram.py
@@ -0,0 +1,8 @@
+import datetime
+
+exampleS = datetime.datetime(2017, 2, 2, 9, 6, 30)
+exampleE = datetime.datetime(2017, 2, 2, 13, 6, 30)
+example2 = datetime.datetime(2017, 2, 4, 10, 35, 42)
+
+rate = 
+FirstDay = exampleE-exampleS
