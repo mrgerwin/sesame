36bfe0986faa4d48d93f1532b827fca495d49270
mrgerwin <jgerwin@otsegoknights.org>
2017-10-12 10:27:11 -0500

    changes 10-16-17

--- NEATTXT.py
+++ NEATTXT.py
@@ -1,55 +1,67 @@
-from tkinter import *
 from time import *
 from datetime import *
 import time
 import math
 import os
 
-midnight = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-now = datetime.now()
-delta = now - midnight
-deltam=int(delta.seconds)//60
-deltah=int(deltam)//60
-deltami=str(datetime.now().strftime("%M"))
-print(str(deltah) + ":" + str(deltami))
+
+HD=8
+MD=60
+H=datetime.now().strftime("%H")
+M=datetime.now().strftime("%M")
+H=int(H)
+M=int(M)
+deltah=HD-H
+deltam=MD-M
+deltah=abs(deltah)
+delta=str(deltah)+":"+str(deltam)
+print(delta)
+print(deltah)
 
 SID=666076
 name='Blayk'
 middleInitinal='R'
 Lname='Sterling'
-#54,900=3:15pm
+grade="2"
+parentNamemale='t'
+parentNamefemale="turte"
+email="bman1920x@aol.com"
+description="LATE"
+rate=3
+charge=deltah*rate
+balance=100
+paid=balance-charge
 
+
+Y=str(datetime.now().strftime("%Y"))
+Y=int(Y)
+years=Y+1
 curtime=time.time()
 curtime=math.floor(curtime)
-print(curtime)
+Date=datetime.now().strftime("%m"+"/"+"%d"+"/"+"%y")
+
 
 def txtapp():
     if(os.path.getsize("testfilemth.txt")==0):
         fin="testfilemth.txt"
         file=open(fin,"w")
-        file.write("==============================================")
-        file.write('\n'+name+" "+middleInitinal+' '+Lname+'\t'+str(SID))
+        file.write(name+" "+middleInitinal+' '+Lname+'\t'+str(SID)+'\n')
+        file.write("Name:"+parentNamemale+" "+parentNamefemale+'\n')
+        file.write("Latch Key"+"\t"+"Grade:"+grade+'\n')
+        file.write("Email: "+email+'\n')
+        file.write("Years: "+str(Y)+"-"+str(years)+'\n')
+        file.write("EIN#34-6401543"+'\n')
+        file.write("==============================================================="+'\n')
+        file.write("KEEP FOR TEXAS!"+'\n')
+        file.write("\n")
+        file.write("#HRS"+"      IN "+"     DESCRIPTION"+"    PER"+"    AMT"+"     AMT"+'\n')
+        file.write("      "+"   DATE "+"                "+"   HOUR"+ "   CHRG"+"    PAID"+"    TOTAL"+'\n')
+        file.write("----------------------------------------------------------------"+'\n')
+        
     fin="testfilemth.txt"
     file=open(fin,"a")
-    file.write('\n'+"Date in: " + str(datetime.now().strftime("%m/%d/%Y")))
+    file.write(" "+str(deltah)+"     "+str(Date)+"      "+description+"         "+"$"+str(rate)+"     "+"$"+str(charge)+ "      $"+str(charge)+"      $"+str(paid)+"\n")
     
-    H=datetime.now().strftime("%H")
-    M=datetime.now().strftime("%M")
-    H=int(H)
-    M=int(M)
     
-    if(H >= 16 and M>=15):
-        
-        file.write('\n'+"Time out: " + str(datetime.now().strftime("%H:%M:%S")))
-        Due=timeSpent*3
-        file.write('\n'+"Money Due: $"+str(Due))
-    
-    elif(H >=9 and H <= 11):
-        timeSpent= 11-H
-        file.write('\n'+"Time in: " + str(datetime.now().strftime("%H:%M:%S")))
-    elif(H <=9):
-        timeSpent=9-H
-        file.write('\n'+"Time in: " + str(datetime.now().strftime("%H:%M:%S")))
-    file.close()
 
 txtapp()
